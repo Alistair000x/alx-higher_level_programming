1cb78c7553376466128ecae271b1bfc22bdc91b2
alistair000x <darmesnun43@gmail.com>
2023-11-13 09:51:12 -0800

    8th

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process');
+const numberOfO = process.argv[2];
+if (!Number.isInteger(Number(numberOfO))) {
+  console.log('Missing number of occurrences');
+ return;
+}
+const numberOfOAsInteger = Number(numberOfO);
+for (let i = 0; i < numberOfOAsInteger; i++) {
+  console.log('C is fun');
+}
+return;
