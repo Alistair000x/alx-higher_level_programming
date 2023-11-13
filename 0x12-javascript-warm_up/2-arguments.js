ce265d486e53e1bd4e2194108eb7ace3e45218cf
alistair000x <darmesnun43@gmail.com>
2023-11-13 06:43:58 -0800

    this is how u can write a script that print message depending of the number of arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ const process = require('process');
 const ArgumentsCalculation = process.argv.length;
 
 if (ArgumentsCalculation === 2) {
-  console.log('Argument found');
-} else if (ArgumentsCalculation > 2) {
-  console.log('Arguments found');
-} else {
   console.log('No argument');
+} else if (ArgumentsCalculation === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
