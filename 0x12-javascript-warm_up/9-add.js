84f4cf89095da89b93ef7fe954d1b72c90c9d6bd
alistair000x <darmesnun43@gmail.com>
2023-11-13 11:31:07 -0800

    10

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
 function add (a, b) {
   return a + b;
 }
-const Argumentf = process.argv[2];
-const Arguments = process.argv[3];
-if (!Number.isInteger(Number(Argumentf)) || !Number.isInteger(Number(Arguments))) {
-  console.log('NaN');
-}
-const ArgumentfAsInteger = Number(Argumentf);
-const ArgumentsAsInteger = Number(Arguments);
-const sum = add(ArgumentfAsInteger, ArgumentsAsInteger);
-console.log(sum);
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
