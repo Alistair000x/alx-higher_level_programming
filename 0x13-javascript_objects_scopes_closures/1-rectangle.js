ec0f2520a2282c38579ab5ec210570e8a837aa2a
Alistair000x <darmensun43@gmail.com>
2023-11-14 23:14:34 +0000

    tasks

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-
-class Rectangle {
-    constructor(h, w) {
+module.exports = class Rectangle {
+    constructor (w, h) {
       this.width = w;
       this.height = h;
     }
-  }
-  
-  module.exports = Rectangle;
+  };
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,6 @@
 #!/usr/bin/node
-
-class Rectangle {
-    constructor(w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      } else {
-        Object.create(null);
-      }
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
     }
-  
-    print() {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-  }
-  
-  module.exports = Rectangle;
-  
+  };
